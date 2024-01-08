13921ef1e8c630c9c64b29fc560f1fb67552b703
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-08 01:48:24 -0300

    🚧 Home page

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+    return (
+        <div className="flex h-full p-2 bg-zinc-800">
+            <div className="flex justify-center w-1/2">
+                <h1>oi</h1>
+            </div>
+            <div className="w-1/2">
+                <h1>oioi</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+const MainContent = () => {
+    return (
+        <main className="flex-1 space-x-5 bg-zinc-950">
+            <div className="bg-zinc-800 rounded-lg h-full">
+                <div className="gap-4">
+                    <h1 className="font-boldtext-3xl mt-10 gap-4 mb-3">Bom dia</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default MainContent
