2606280fd6e70ffc8842a3a8043e36bcec75c6f9
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-18 20:03:49 -0300

    🚧 Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 const Navbar = () => {
     return (
-        <div>
-            oi
-        </div>
+        <nav className="mb-3">
+            <div className="flex gap-16 justify-center">
+                <ul>
+                    <div className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
+                        Sobre
+                    </div>
+                </ul>
+                <ul>
+                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
+                        Habilidades
+                    </div>
+                </ul>
+                <ul>
+                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
+                        Projetos
+                    </div>
+                </ul>
+                <ul>
+                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
+                        Contato
+                    </div>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
