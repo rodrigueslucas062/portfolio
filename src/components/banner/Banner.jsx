36dc7dee29c57d3d43fb1d1454305fe6b6c7d45a
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-18 22:09:05 -0300

    💄🚧 Banner introduction

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,14 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
 const Banner = () => {
+    const [clicked, setClicked] = useState(false);
+
+    useEffect(() => {
+        // Adiciona a classe 'clicked' assim que o componente é montado
+        setClicked(true);
+    }, []);
+
     return (
-        <section className="flex gap-4">
+        <section className="flex gap-4 my-18">
             <div className="w-1/2">
-                <h1>oioi</h1>
+                <div id="text-drop" className={clicked ? 'clicked flex' : ''}>
+                    <div className="a">A</div>
+                    <div className="l">l</div>
+                    <div className="o">o</div>
+                    <div className="u">u</div>
+                    <div className="smile">:)</div>
+                </div>
+                <h1 className='strong'></h1>
             </div>
             <div className="w-1/2">
-                <h2>oioi</h2>
+                <h2>Alguma imagem bacana que combine com o site</h2>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Banner;
