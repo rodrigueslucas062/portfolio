8b1dbd8cf9b66277a63b19fc16933baa647aa627
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-23 22:23:54 -0300

    ✨ Content update

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -1,11 +1,11 @@
-import { GraduationCap } from "lucide-react";
+import { Briefcase, GraduationCap } from "lucide-react";
 import Image from "next/image";
 
 const Sobre = () => {
     const avatarUrl = 'https://avatars.githubusercontent.com/u/39422637?s=400&u=b6bddbc97edde098ad4d50599d99479aff0cb6d9&v=4';
     return (
         <div className="flex my-10 gap-4">
-            <div className="flex flex-1 w-1/3">
+            <div className="flex group flex-1 w-1/3">
                 <div className="grid grid-cols-1 md:grid-cols-2 w-full bg-white/5 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10">
                     <div className="flex flex-col items-center py-10">
                         <Image className="mb-3 rounded-full shadow-lg border-2 p-1 border-gray-600" src={avatarUrl} alt="Lucas Rodrigues" width={125} height={125} />
@@ -14,47 +14,44 @@ const Sobre = () => {
                     </div>
                     <div className="flex flex-col py-10 px-4 space-y-4">
                         <h1 className="text-3xl font-bold text-white">Qualificações</h1>
-                        <div className="flex gap-2">
+                        <div className="flex gap-2 items-center">
                             <GraduationCap />
                             <span className="text-sm text-gray-400">Sistemas de informação</span>
                         </div>
-                        <div className="flex gap-2">
-                            <GraduationCap />
+                        <div className="flex gap-2 items-center">
+                            <Briefcase />
                             <span className="text-sm text-gray-400">Web designer</span>
                         </div>
-                        <div className="flex gap-2">
-                            <GraduationCap />
+                        <div className="flex gap-2 items-center">
+                            <Briefcase />
                             <span className="text-sm text-gray-400">UX/UI</span>
                         </div>
                     </div>
-                    <div className="py-4 px-10">
-                        <h1 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Tecnologias</h1>
-                        <div className="flex flex-col space-y-2">
-                            <div className="flex flex-col-5 gap-4">
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">HTML</span>
-                                </div>
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">CSS</span>
-                                </div>
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">JavaScript</span>
+                    <div className="px-10 space-y-4 w-full">
+                        <h1 className="mb-4 text-xl font-bold text-white">Idiomas</h1>
+                        <div>
+                            <div className="flex justify-between mb-2">
+                                <span className="text-base font-medium text-white">Português</span>
+                                <span className="text-base font-medium text-white">Nativo</span>
+                            </div>
+                            <div className="flex flex-col border-2 border-gray-700 group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
+                                <div className="w-full rounded-full h-2.5 bg-purple-950">
+                                    <div className="bg-purple-700 h-2.5 rounded-full w-full"></div>
                                 </div>
                             </div>
-                            <div className="ml-2 flex flex-col-3 gap-6">
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">React</span>
-                                </div>
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">Next.js</span>
-                                </div>
-                                <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
-                                    <span className="text-xs text-gray-400 px-4 ">Figma</span>
+                        </div>
+                        <div>
+                            <div className="flex justify-between mb-2">
+                                <span className="text-base font-medium text-white">Inglês</span>
+                                <span className="text-base font-medium text-white">Intermediario</span>
+                            </div>
+                            <div className="flex flex-col border-2 border-gray-700 group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
+                                <div className="w-full rounded-full h-2.5 bg-purple-950">
+                                    <div className="bg-purple-700 h-2.5 rounded-full w-1/2"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
             <div className="flex flex-1 group w-full max-w-xl bg-white/5 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10 p-4">
