effa44fd05aa5596a1535e1f1be27c9508d0c495
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-26 00:57:58 -0300

    🚸Color palette and accessibility improvements

--- src/components/habilidades/Habilidades.jsx
+++ src/components/habilidades/Habilidades.jsx
@@ -6,27 +6,27 @@ const Habilidades = () => {
                     <h1 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Tecnologias</h1>
                     <div className="flex flex-col space-y-2">
                         <div className="flex flex-col-5 gap-4 justify-between">
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">HTML</span>
                             </div>
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">CSS</span>
                             </div>
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">JavaScript</span>
                             </div>
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">JavaScript</span>
                             </div>
                         </div>
                         <div className="flex flex-col-5 gap-4 justify-between">
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">React</span>
                             </div>
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">Next.js</span>
                             </div>
-                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-gray-700 flex items-center justify-center">
+                            <div className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
                                 <span className="text-xs text-gray-400 px-4 ">Figma</span>
                             </div>
                         </div>
@@ -41,9 +41,9 @@ const Habilidades = () => {
                             <span className="text-base font-medium text-white">Português</span>
                             <span className="text-base font-medium text-white">Nativo</span>
                         </div>
-                        <div className="flex flex-col border-2 border-gray-700 group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
-                            <div className="w-full rounded-full h-2.5 bg-purple-950">
-                                <div className="bg-purple-700 h-2.5 rounded-full w-full"></div>
+                        <div className="flex flex-col border-2 border-azul-claro group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
+                            <div className="w-full rounded-full h-2.5 bg-azul-petroleo">
+                                <div className="bg-azul-claro h-2.5 rounded-full w-full"></div>
                             </div>
                         </div>
                     </div>
@@ -52,9 +52,9 @@ const Habilidades = () => {
                             <span className="text-base font-medium text-white">Inglês</span>
                             <span className="text-base font-medium text-white">Intermediario</span>
                         </div>
-                        <div className="flex flex-col border-2 border-gray-700 group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
-                            <div className="w-full rounded-full h-2.5 bg-purple-950">
-                                <div className="bg-purple-700 h-2.5 rounded-full w-1/2"></div>
+                        <div className="flex flex-col border-2 border-azul-claro group-hover:border-gray-500 rounded-full justify-between mb-1 p-1">
+                            <div className="w-full rounded-full h-2.5 bg-azul-petroleo">
+                                <div className="bg-azul-claro h-2.5 rounded-full w-1/2"></div>
                             </div>
                         </div>
                     </div>
@@ -62,12 +62,13 @@ const Habilidades = () => {
             </div>
             <div className="flex flex-1 w-1/3">
                 <div className="flex flex-col flex-1 group w-full max-w-xl bg-white/5 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10 p-4">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col w-full">
                         <h1>Não sei oque por aqui</h1>
                         <h1>Escolher paleta de cor alem do cinza</h1>
-                        <h1>Trabelhar responsividade</h1>
+                        <h1>Trabalhar responsividade</h1>
                         <h1>Adicionar conteudo em projetos</h1>
                         <h1>certificações</h1>
+                        <h1>texto enormemente enorme so pra encher o tamanho da div pra testar uma coisa que não consigo entender pq nao da certo aaaaa</h1>
                     </div>
                 </div>
             </div>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -10,33 +10,33 @@ const MainContent = () => {
                     <div className="grid grid-cols-6 gap-4 mt-4">
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                            <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <strong className="font-semi-bold">Portifólio</strong>
+                            <span className="text-sm text-gray-400">Elaboração de site portifólio</span>
                         </a>
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
                             <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
                         </a>
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
                             <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
                         </a>
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
                             <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
                         </a>
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
                             <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
                         </a>
                         <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                             <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
                             <strong className="font-semi-bold">Aderal</strong>
-                            <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                            <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
                         </a>
                     </div>
                 </div>
