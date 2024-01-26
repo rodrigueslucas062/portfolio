a25a01e40be4391ac56cae10175a3e7976c570ff
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-25 23:02:54 -0300

    💄Custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'azul-claro': '#265C8C',
+        'azul-escuro': '#052A59',
+        'azul-petroleo': '#054674',
+        'roxo': '#8C2B86',
+        'outro-roxo': '#8A278D',
+        'rosa': '#D93B92',
+        'outro-rosa': '#F0028D',
+       }
     },
   },
   plugins: [],
