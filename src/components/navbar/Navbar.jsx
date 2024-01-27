cb67d46a4351c1c5d0a49a552d76789e8275483c
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-01-27 00:32:27 -0300

    💄New Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,23 @@
 const Navbar = () => {
     return (
-        <nav className="mb-3">
-            <div className="flex gap-16 justify-center">
-                <ul>
-                    <div className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
-                        Sobre
-                    </div>
-                </ul>
-                <ul>
-                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
-                        Habilidades
-                    </div>
-                </ul>
-                <ul>
-                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
-                        Projetos
-                    </div>
-                </ul>
-                <ul>
-                    <div className="bg-white/5 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">
-                        Contato
-                    </div>
-                </ul>
+        <nav className="flex mb-3 gap-16 justify-center">
+            <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                        <li>
+                            <a href="#" className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10"> Sobre</a>
+                        </li>
+                        <li>
+                            <a href="#" className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">Habilidades</a>
+                        </li>
+                        <li>
+                            <a href="#" className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">Projetos</a>
+                        </li>
+                        <li>
+                            <a href="#" className="bg-white/5 px-6 p-3 rounded-xl flex flex-col gap-2 hover:bg-white/10">Contato</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
