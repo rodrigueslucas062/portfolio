179f559ab711cc545047a416c7afdab5f2be890b
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-01 22:46:02 -0300

    💄 Small interface adjustments

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -82,7 +82,7 @@ const Sobre = () => {
                                 <Image className="mb-3 rounded-full shadow-lg border-2 p-1 border-azul-claro" src={avatarUrl} alt="Lucas Rodrigues" width={125} height={125} />
                                 <h5 className="mb-1 text-xl font-medium text-white">Lucas Rodrigues</h5>
                                 <span className="text-sm mb-6 text-gray-400">Desenvolvedor Front-end</span>
-                                <button className="border-2 bg-zinc-900 py-2 px-6 rounded-full">Download CV</button>
+                                <button className="border-2 border-azul-claro bg-zinc-900 py-2 px-6 rounded-full">Download CV</button>
                             </div>
                         </div>
                         <div>
@@ -122,16 +122,16 @@ const Sobre = () => {
                     </article>
                 </div>
                 <div className="hidden md:flex flex-1 w-1/3">
-                    <div className="group w-full p-4 bg-white/5 space-y-4 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10">
+                    <div className="group w-full p-4 bg-white/5 space-y-2 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10">
                         <h1 className="text-3xl font-bold text-white">Experiências</h1>
                         <ol className="relative border-s border-gray-700 group-hover:border-gray-500">
-                            <li className="mb-6 ms-4">
+                            <li className="mb-4 ms-4">
                                 <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
                                 <time className="mb-1 text-sm font-normal leading-none text-gray-400">RentzApp, Mai/2023 - Nov/2023</time>
                                 <h3 className="text-lg font-semibold text-white">Desenvolvedor Front-end</h3>
                                 <p className="mb-4 text-sm font-normal text-gray-400">Atuei utilizando React e Next.js na criação de componentes, páginas estáticas e dinâmicas, integração com APIs GraphQL para a listagem de produtos, e funcionalidades SEO.</p>
                             </li>
-                            <li className="mb-6 ms-4">
+                            <li className="mb-4 ms-4">
                                 <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
                                 <time className="mb-1 text-sm font-normal leading-none text-gray-400">Dunning, Abr/2022 - Nov/2022</time>
                                 <h3 className="text-lg font-semibold text-white">Desenvolvedor Full-stack</h3>
