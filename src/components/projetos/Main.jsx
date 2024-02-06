6ce6466aa38fd951f6d8ed4d3cee3aa4bfc78ec7
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-05 22:05:42 -0300

    ✨ Finalization of cards with projects

--- src/components/projetos/Main.jsx
+++ src/components/projetos/Main.jsx
@@ -1,5 +1,3 @@
-import Image from "next/image"
-import Curtidas from "../../../public/images/curtidas.jpg"
 import ProjetosGrid from "./projetos"
 
 const MainContent = () => {
@@ -9,31 +7,7 @@ const MainContent = () => {
             <div className="md:hidden gap-2">
                 <h2 className="font-bold text-2xl my-10">Projetos</h2>
                 <div className="flex flex-col gap-4 mt-4">
-                    <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                        <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                        <strong className="font-semi-bold">Portifólio</strong>
-                        <span className="text-sm text-gray-400">Projeto de portifólio</span>
-                    </div>
-                    <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                        <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                        <strong className="font-semi-bold">Aderal</strong>
-                        <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
-                    </div>
-                    <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                        <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                        <strong className="font-semi-bold">Aderal</strong>
-                        <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
-                    </div>
-                    <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                        <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                        <strong className="font-semi-bold">Aderal</strong>
-                        <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
-                    </div>
-                    <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                        <Image className="w-auto" src={Curtidas} alt="Capa playlist" width={190} height={190} />
-                        <strong className="font-semi-bold">Aderal</strong>
-                        <span className="text-sm text-gray-400">Aderal, de Slipknot</span>
-                    </div>
+                    <ProjetosGrid />
                 </div>
             </div>
 
--- src/components/projetos/projetos.js
+++ src/components/projetos/projetos.js
@@ -1,23 +1,40 @@
 import React from "react";
 import Image from "next/image";
 import Curtidas from "../../../public/images/curtidas.jpg";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 
 const informacoes = [
   {
-    href: "link-para-o-projeto-1",
-    imagemSrc: { Curtidas },
-    imagemAlt: "Descrição da imagem 1",
+    code: "https://github.com/rodrigueslucas062/portfolio",
+    demo:"https://lucas-rodrigues-portfolio.vercel.app/",
+    imagemSrc: Curtidas,
+    imagemAlt: "Imagem de portifolio",
     titulo: "Portifólio",
     descricao: "Projeto de portifólio",
   },
 ];
 
-const Projetos = ({ imagemSrc, imagemAlt, titulo, descricao }) => {
+const Projetos = ({ code, demo, imagemSrc, imagemAlt, titulo, descricao }) => {
   return (
     <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
       <Image className="w-auto" src={imagemSrc} alt={imagemAlt} width={190} height={190} />
       <strong className="font-semi-bold">{titulo}</strong>
       <span className="text-sm text-gray-400">{descricao}</span>
+      <div className="flex gap-4 items-center">
+        <div className=" bg-zinc-900 rounded-2xl border-2 border-azul-claro">
+          <Link href={code} target="_blank" className="flex justify-center items-center py-2 px-3 gap-2 text-white">
+            <Github size={16} />
+            <span>Code</span>
+          </Link>
+        </div>
+        <div className=" bg-zinc-900 rounded-2xl border-2 border-azul-claro">
+        <Link href={demo} target="_blank" className="flex justify-center items-center py-2 px-3 gap-2 text-white">
+            <ExternalLink size={16} />
+            <span>Demo</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
@@ -28,6 +45,8 @@ const ProjetosGrid = () => {
       {informacoes.map((info, index) => (
         <Projetos
           key={index}
+          code={info.code}
+          demo={info.demo}
           imagemSrc={info.imagemSrc}
           imagemAlt={info.imagemAlt}
           titulo={info.titulo}
