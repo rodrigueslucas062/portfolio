c528e2d5a73e6e7adb6202e179b24fe5bea467ca
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-13 01:17:24 -0300

    💄 New tag order

--- src/components/habilidades/tags.js
+++ src/components/habilidades/tags.js
@@ -1,13 +1,13 @@
 const tags = [
+  "Next.js",
+  "React",
+  "Figma*",
+  "Web Design*",
   "HTML",
   "JavaScript",
   "UX/UI*",
-  "Web Design*",
-  "Experiência do Usuário*",
+  "User Experience*",
   "CSS",
-  "React",
-  "Next.js",
-  "Figma*",
   "Tailwind",
 ];
 
@@ -17,8 +17,7 @@ const TagsGrid = () => {
       {tags.map((tag, index) => (
         <div
           key={index}
-          className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center"
-        >
+          className="bg-zinc-800 p-2 px-4 rounded-2xl border-2 border-azul-claro flex items-center justify-center">
           <span className="text-xs text-gray-400 px-4">{tag}</span>
         </div>
       ))}
