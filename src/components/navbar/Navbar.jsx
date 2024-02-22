c8f2a1ca42d95cfee17255c5cbb13944c03adbf0
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-21 21:17:23 -0300

    🩹 Correct button position on the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,19 +26,19 @@ export const Navbar = () => {
 
     return (
         <nav className="bg-zinc-900 fixed w-full z-20 top-0 start-0">
+            <div className="flex justify-end md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button className="inline-flex items-center p-2 w-10 h-10 mt-4 mr-4 justify-center text-sm text-gray-400 rounded-lg md:hidden bg-zinc-100"
+                    aria-controls="navbar-sticky"
+                    aria-expanded="false"
+                    data-collapse-toggle="navbar-sticky"
+                    type="button"
+                    onClick={toggleMenu}>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+            </div>
             <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-3">
-                <div className="flex justify-end md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden bg-zinc-100"
-                        aria-controls="navbar-sticky"
-                        aria-expanded="false"
-                        data-collapse-toggle="navbar-sticky"
-                        type="button"
-                        onClick={toggleMenu}>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
-                </div>
                 <div className={`${isMenuOpen ? 'block' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 font-medium max-md:space-y-2 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:border-0">
                         <li>
