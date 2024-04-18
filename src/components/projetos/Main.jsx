140c01cff27fd17eb223bcdea4e91ee263a94c6b
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-04-18 11:25:19 -0300

    🎨♻️ Layout refactoring

--- src/components/projetos/Main.jsx
+++ src/components/projetos/Main.jsx
@@ -1,23 +1,122 @@
 import ProjetosGrid from "./projetos"
+import { useEffect, useRef } from "react";
+
 
 const MainContent = () => {
-    return (
-        <section className="flex-1 space-x-5">
-            <div className="md:hidden gap-2">
-                <h2 className="font-bold text-2xl my-10">Projetos</h2>
-                <div className="flex flex-col gap-4 mt-4">
-                    <ProjetosGrid />
-                </div>
-            </div>
+    const svgRef = useRef(null);
 
-            <div className="hidden md:block gap-4">
-                <h2 className="font-bold text-2xl mt-10 mb-4">Projetos</h2>
-                <span>Clique no card para mais informações</span>
-                <div className="grid grid-cols-6 gap-4 mt-4">
-                    <ProjetosGrid />
+    useEffect(() => {
+        const handleResize = () => {
+            const svg = svgRef.current;
+            if (svg) {
+                const rect = svg.getBoundingClientRect();
+                const width = rect.width;
+                const height = rect.height;
+                svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+            }
+        };
+
+        const handleScroll = () => {
+            handleResize();
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <section className="flex-1 py-4 space-x-5 px-4 2xl:px-0">
+                <div className="md:hidden gap-2">
+                    <h2 className="font-bold text-2xl my-5">Projetos</h2>
+                    <div className="flex flex-col gap-4 mt-4">
+                        <ProjetosGrid />
+                    </div>
                 </div>
-            </div>
-        </section>
+
+                <div className="hidden md:block gap-4">
+                    <h2 className="font-bold text-2xl mb-4">Projetos</h2>
+                    <span>Clique no card para mais informações</span>
+                    <div className="flex gap-8 mt-4">
+                        <ProjetosGrid />
+                    </div>
+                </div>
+            </section>
+            <svg
+                ref={svgRef}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1920 1080"
+                preserveAspectRatio="xMinYMin slice"
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                    top: 0,
+                    left: 0,
+                    zIndex: -1,
+                }}
+            >
+                <defs>
+                    <path
+                        id="selector-zigzag-path"
+                        d="M-5 5L5.1 15 15 5l10 10"
+                        strokeLinecap="square"
+                        strokeWidth="7"
+                        stroke="#3d444b"
+                        fill="none"
+                    />
+                    <pattern
+                        id="selector-zigzag-bg"
+                        patternUnits="userSpaceOnUse"
+                        width="20"
+                        height="20"
+                        patternTransform="scale(2.5)"
+                    >
+                        <g id="selector-zigzag-animated-group">
+                            <g>
+                                <use href="#selector-zigzag-path" />
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="translate"
+                                    keyTimes="0;1"
+                                    repeatCount="indefinite"
+                                    dur="1.5s"
+                                    values="0 0; 0 20"
+                                />
+                            </g>
+                        </g>
+                        <g
+                            id="selector-zigzag-animated-group"
+                            transform="translate(0, -20)"
+                        >
+                            <g>
+                                <use href="#selector-zigzag-path" />
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="translate"
+                                    keyTimes="0;1"
+                                    repeatCount="indefinite"
+                                    dur="1.5s"
+                                    values="0 0; 0 20"
+                                />
+                            </g>
+                        </g>
+                    </pattern>
+                </defs>
+                <rect width="100%" height="100%" fill="#65696F" />
+                <rect
+                    width="100%"
+                    height="100%"
+                    transform="translate(0,0)"
+                    fill="url(#selector-zigzag-bg)"
+                />
+            </svg>
+        </>
     )
 }
 
--- src/components/projetos/projetos.js
+++ src/components/projetos/projetos.js
@@ -23,7 +23,7 @@ const informacoes = [
     imagemSrc: Curtidas,
     imagemAlt: "Imagem do projeto",
     titulo: "Spotify interface",
-    descricao: "Construção da home do Spotify",
+    descricao: "Home do Spotify",
     descricao_modal: "Descrição do projeto Spotify interface",
     tecnologias: ["React", "Next.js", "CSS", "HTML", "JavaScript"],
   },
@@ -33,7 +33,7 @@ const informacoes = [
     imagemSrc: Curtidas,
     imagemAlt: "Imagem do projeto",
     titulo: "Bloco de anotações",
-    descricao: "Um projeto de anotações criadas por voz",
+    descricao: "ToDo list com anotações",
     descricao_modal: "Descrição do projeto Bloco de anotações",
     tecnologias: ["Next.js", "CSS", "HTML", "JavaScript", "Tailwind", "SpeechRecognitionAPI"],
   },
@@ -52,28 +52,24 @@ const informacoes = [
 const Projetos = ({ code, demo, imagemSrc, imagemAlt, titulo, descricao, descricao_modal, tecnologias }) => {
   return (
     <Dialog.Root>
-      <Dialog.Trigger>
-        <div className="bg-white/5 p-3 text-left rounded-md flex flex-col gap-2 hover:bg-white/10">
-          <Image
-            className="w-auto"
-            src={imagemSrc}
-            alt={imagemAlt}
-            width={190}
-            height={190}
-          />
-          <strong className="font-semi-bold">{titulo}</strong>
-          <span className="text-sm text-gray-400">{descricao}</span>
-          <div className="flex gap-4 items-center">
-            <div className=" bg-zinc-900 rounded-2xl border-2 border-azul-claro">
-              <Link href={code} target="_blank" className="flex justify-center items-center py-2 px-3 gap-2 text-white">
+      <Dialog.Trigger className="flex flex-1">
+        <div className="flex bg-zinc-50/20 backdrop-blur-sm p-3 text-left rounded-md gap-2 border-2 border-azul-claro">
+          <Image src={imagemSrc} alt={imagemAlt} max-width={100} height={100} className="rounded-md" />
+          <div className="flex flex-col space-y-3">
+            <strong className="font-semi-bold">{titulo}</strong>
+            <span className="text-sm font-semibold text-white">{descricao}</span>
+            <div className="flex gap-2 items-center">
+              <Link href={code} target="_blank"
+                className="flex bg-zinc-900 rounded-md border-2 border-azul-claro justify-center items-center p-2 gap-2 text-white" 
+                onClick={(e) => e.stopPropagation()}>
                 <Github size={16} />
-                <span>Code</span>
+                <span className="text-sm">Code</span>
               </Link>
-            </div>
-            <div className=" bg-zinc-900 rounded-2xl border-2 border-azul-claro">
-              <Link href={demo} target="_blank" className="flex justify-center items-center py-2 px-3 gap-2 text-white">
+              <Link href={demo} target="_blank"
+                className="flex bg-zinc-900 rounded-md border-2 border-azul-claro justify-center items-center p-2 gap-2 text-white" 
+                onClick={(e) => e.stopPropagation()}>
                 <ExternalLink size={16} />
-                <span>Demo</span>
+                <span className="text-sm">Demo</span>
               </Link>
             </div>
           </div>
@@ -82,25 +78,12 @@ const Projetos = ({ code, demo, imagemSrc, imagemAlt, titulo, descricao, descric
 
       <Dialog.Portal>
         <Dialog.DialogOverlay className="inset-0 fixed bg-black/90">
-          <DialogContent className="fixed z-50 p-8 inset-0 bg-zinc-800 border-2 border-gray-700 rounded-2xl md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[940px] w-full md:h-[60vh] flex flex-col outline-none overflow-hidden">
+          <DialogContent className="fixed z-50 p-8 inset-0 bg-zinc-800 border-2 border-gray-700 rounded-2xl md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[940px] w-full md:h-[80vh] flex flex-col outline-none overflow-hidden">
             <Dialog.Close className="absolute right-0 top-0 p-1.5 text-gray-400 hover:text-gray-100 rounded-xl">
               <X size={20} />
             </Dialog.Close>
             <div className="flex-1 flex gap-8">
-              <div className="space-y-4 lg:w-1/2">
-                <p className="text-gray-400">{descricao_modal}</p>
-                <div className="border-2 border-gray-700 space-y-3">
-                  <h1 className="text-bold text-xl">Tecnologias usadas</h1>
-                  <TagsGrid tecnologias={tecnologias} />
-                </div>
-              </div>
-              <div className="space-y-4 lg:w-1/2">
-                <p className="text-gray-400">
-                  Criei esse portifólio com o intuito de mostrar outros projetos
-                  que criei
-                </p>
-                <h1 className="text-bold text-md">Tecnologias usadas</h1>
-              </div>
+              <h1>oi</h1>
             </div>
           </DialogContent>
         </Dialog.DialogOverlay>
@@ -109,17 +92,20 @@ const Projetos = ({ code, demo, imagemSrc, imagemAlt, titulo, descricao, descric
   );
 };
 
-const TagsGrid = ({ tecnologias }) => {
-  return (
-    <div className="flex flex-wrap gap-2">
-      {tecnologias.map((tag, index) => (
-        <div key={index} className="bg-zinc-900 p-2 rounded-md border-2 border-azul-claro flex items-center justify-center">
-          <span className="text-xs text-gray-400 px-2">{tag}</span>
-        </div>
-      ))}
-    </div>
-  );
-};
+// const TagsGrid = ({ tecnologias }) => {
+//   return (
+//     <div className="flex flex-wrap gap-2">
+//       {tecnologias.map((tag, index) => (
+//         <div
+//           key={index}
+//           className="bg-zinc-900 p-2 rounded-md border-2 border-azul-claro flex items-center justify-center"
+//         >
+//           <span className="text-xs text-gray-400 px-2">{tag}</span>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
 const ProjetosGrid = () => {
   return (
