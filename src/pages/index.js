690a8dc1faa667b0ced338a96bb771d74bfc7695
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-24 20:46:31 -0300

    🗑️ Deprecate code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Banner from "@/components/banner/Banner";
 import Contato from "@/components/contato/Contato";
-import Habilidades from "@/components/habilidades/Habilidades";
 import MainContent from "@/components/projetos/Main";
 import Navbar from "@/components/navbar/Navbar";
 import Sobre from "@/components/sobre/Sobre";
@@ -12,7 +11,6 @@ export default function Home() {
       <Banner />
       <div className="flex flex-col flex-1 bg-zinc-800 p-4 md:p-8">
         <Sobre />
-        <Habilidades />
       </div>
       <div>
         <MainContent />
