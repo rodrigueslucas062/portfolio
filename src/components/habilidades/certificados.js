039c91b6386aede81aa6cd501a7c5ff29713fdfc
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-24 20:47:20 -0300

    🚸 Improvements in the arrangement of items

--- src/components/habilidades/certificados.js
+++ src/components/habilidades/certificados.js
@@ -1,9 +1,10 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const informacoes = [
   {
     link: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/ScrumFundamentalsCertified-LucasRodrigues-922080.pdf",
-    titulo: " Certificado em Scrum",
+    titulo: "Certificado em Scrum",
     descricao: "SCRUMstudy - Accreditation Body for Scrum and Agile",
   },
   {
@@ -11,25 +12,64 @@ const informacoes = [
     titulo: "NLW Expert trilha de React",
     descricao: "Rocketseat",
   },
+  {
+    link: "https://app.rocketseat.com.br/certificates/b083423a-d2e9-4f33-98dc-eddf3cfa7a8e",
+    titulo: "NLW Unite - React Native",
+    descricao: "Rocketseat",
+  },
+  {
+    link: "https://app.rocketseat.com.br/certificates/ef0ffad9-2995-49ef-bf93-4c514b053856",
+    titulo: "NLW Unite - Reactjs",
+    descricao: "Rocketseat",
+  },
+  {
+    link: "",
+    titulo: "Análise de dados e big data",
+    descricao: "Centro Universitário Una",
+  },
+  {
+    link: "",
+    titulo: "Design Thinking e Inovação",
+    descricao: "Centro Universitário Una",
+  },
 ];
 
 const Certificados = ({ link, titulo, descricao }) => {
   return (
-    <Link href={link}
-      className="bg-zinc-800 py-2 px-6 rounded-2xl border-2 border-azul-claro flex items-center" target="_blank">
-      <div className="flex flex-col">
-        <strong className="font-semi-bold">
-          {titulo}
-        </strong>
-        <span className="text-sm text-gray-400">
-          {descricao}
-        </span>
+    <Link
+      href={link}
+      target="_blank"
+      className="bg-zinc-800 py-2 px-6 rounded-2xl border-2 border-azul-claro flex items-center"
+    >
+      <div className="flex flex-col space">
+        <strong className="font-semi-bold">{titulo}</strong>
+        <span className="text-sm text-gray-400">{descricao}</span>
       </div>
     </Link>
   );
 };
 
-const CertificadosGrid = () => {
+export function CertificadosGrid() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % informacoes.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Certificados
+      link={informacoes[currentIndex].link}
+      titulo={informacoes[currentIndex].titulo}
+      descricao={informacoes[currentIndex].descricao}
+    />
+  );
+}
+
+export function CertificadosGridModal() {
   return (
     <>
       {informacoes.map((info, index) => (
@@ -42,6 +82,4 @@ const CertificadosGrid = () => {
       ))}
     </>
   );
-};
-
-export default CertificadosGrid;
+}
--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -1,43 +1,112 @@
+import { ExternalLink, X } from "lucide-react";
+import TagsGrid from "../habilidades/tags";
+import * as Dialog from "@radix-ui/react-dialog";
+import { CertificadosGrid, CertificadosGridModal } from "../habilidades/certificados";
+
 const Sobre = () => {
     return (
         <section id="sobre" className="flex max-lg:flex-col gap-4 bg-zinc-800">
             <div className="flex flex-1">
-                <div className="group w-full p-4 bg-white/5 space-y-2 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10">
-                    <article className="text-wrap">
-                        <h1 className="text-2xl font-bold text-white mb-4">Educação</h1>
-                        <ol className="relative border-s border-gray-700 group-hover:border-gray-500">
-                            <li className="mb-4 ms-4">
-                                <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
-                                <h3 className="text-lg font-semibold text-white">Bacharel em Sistemas de Informação</h3>
-                                <time className="mb-1 text-sm font-normal leading-none text-gray-400">Fev/2021 - Dez/2024</time>
-                            </li>
-                            <li className="mb-4 ms-4">
-                                <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
-                                <h3 className="text-lg font-semibold text-white">Colégio Cotemig</h3>
-                                <time className="mb-1 text-sm font-normal leading-none text-gray-400">Fev/2017 - Dez/2019</time>
-                            </li>
-                        </ol>
-                    </article>
+                <div className="group w-full p-4 bg-white/5 space-y-2 rounded-xl border-2 border-gray-700 hover:bg-white/10">
+                    <div className="max-md:flex-col flex gap-4 justify-around">
+                        <article className="text-wrap lg:w-1/2 bg-zinc-900 p-4 rounded-lg">
+                            <h1 className="text-2xl font-bold text-white mb-4">Educação</h1>
+                            <ol className="relative border-s border-azul-claro">
+                                <li className="mb-4 ms-4">
+                                    <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-azul-escuro bg-azul-claro"></div>
+                                    <h3 className="text-lg font-semibold text-white">Bacharel em Sistemas de Informação</h3>
+                                    <time className="mb-1 text-sm font-normal leading-none text-gray-400">Fev/2021 - Dez/2024</time>
+                                </li>
+                                <li className="mb-4 ms-4">
+                                    <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-azul-escuro bg-azul-claro"></div>
+                                    <h3 className="text-lg font-semibold text-white">Colégio Cotemig</h3>
+                                    <time className="mb-1 text-sm font-normal leading-none text-gray-400">Fev/2017 - Dez/2019</time>
+                                </li>
+                            </ol>
+                        </article>
+                        <div className="w-full lg:w-1/2 space-y-4 bg-zinc-900 p-4 rounded-lg">
+                            <h1 className="mb-4 text-xl font-bold text-white">Idiomas</h1>
+                            <div>
+                                <div className="flex justify-between mb-2">
+                                    <span className="text-base font-medium text-white">Português</span>
+                                    <span className="text-base font-medium text-white">Nativo</span>
+                                </div>
+                                <div className="flex flex-col border-2 border-azul-claro rounded-full justify-between mb-1 p-1">
+                                    <div className="w-full rounded-full h-2.5 bg-azul-petroleo">
+                                        <div className="bg-azul-claro h-2.5 rounded-full w-full"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex justify-between mb-2">
+                                    <span className="text-base font-medium text-white">Inglês</span>
+                                    <span className="text-base font-medium text-white">Intermediario</span>
+                                </div>
+                                <div className="flex flex-col border-2 border-azul-claro rounded-full justify-between mb-1 p-1">
+                                    <div className="w-full rounded-full h-2.5 bg-azul-petroleo">
+                                        <div className="bg-azul-claro h-2.5 rounded-full w-1/2"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="max-md:flex-col flex gap-4">
+                        <div className="flex flex-col lg:w-1/2 bg-zinc-900 rounded-lg p-4">
+                            <h1 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Tecnologias</h1>
+                            <TagsGrid />
+                        </div>
+                        <div className="flex flex-col lg:w-1/2 bg-zinc-900 rounded-lg p-4 space-y-4">
+                            <div className="flex justify-between">
+                                <h1 className="text-xl font-bold text-white">Certificados</h1>
+                                <Dialog.Root>
+                                    <Dialog.Trigger>
+                                        <div className="text-sm text-gray-400 flex gap-2 items-center hover:bg-zinc-800 px-2 py-1 rounded-md hover:ring-1 ring-azul-claro">
+                                            Exibir todos
+                                            <ExternalLink size={12} />
+                                        </div>
+                                    </Dialog.Trigger>
+
+                                    <Dialog.Portal>
+                                        <Dialog.DialogOverlay className="inset-0 fixed bg-black/20">
+                                            <Dialog.DialogContent className="fixed z-10 inset-0 md:inset-auto max-md:top-[40%] lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-zinc-800 max-md:rounded-t-3xl lg:rounded-3xl flex flex-col outline-none overflow-hidden">
+                                                <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2 hover:text-zinc-900 text-gray-200">
+                                                    <X className="size-5" />
+                                                </Dialog.Close>
+                                                <div className="flex justify-center p-4">
+                                                    <div className="rounded-lg mt-8 inline-block w-5/6 space-y-3">
+                                                        <h2 className="font-semibold text-center text-lg">Certificados</h2>
+                                                        <CertificadosGridModal />
+                                                    </div>
+                                                </div>
+                                            </Dialog.DialogContent>
+                                        </Dialog.DialogOverlay>
+                                    </Dialog.Portal>
+
+                                </Dialog.Root>
+                            </div>
+                            <CertificadosGrid />
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="flex flex-1">
                 <div className="group w-full p-4 bg-white/5 space-y-2 rounded-xl shadow border-2 border-gray-700 hover:bg-white/10">
                     <h1 className="text-3xl font-bold text-white">Experiências</h1>
-                    <ol className="relative border-s border-gray-700 group-hover:border-gray-500">
+                    <ol className="relative border-s border-azul-claro">
                         <li className="mb-4 ms-4">
-                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-azul-claro"></div>
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400">RentzApp, Mai/2023 - Dez/2023</time>
                             <h3 className="text-lg font-semibold text-white">Desenvolvedor Front-end</h3>
                             <p className="mb-4 text-sm font-normal text-gray-400">Atuei utilizando React e Next.js na criação de componentes, páginas estáticas e dinâmicas, integração com APIs GraphQL para a listagem de produtos, e funcionalidades SEO.</p>
                         </li>
                         <li className="mb-4 ms-4">
-                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-azul-claro"></div>
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400">Dunning, Abr/2022 - Nov/2022</time>
                             <h3 className="text-lg font-semibold text-white">Desenvolvedor Full-stack</h3>
                             <p className="text-sm font-normal text-gray-400">Desempenhei um papel na manutenção do banco de dados, implementação de melhorias com Java e Html, e na documentação e teste das APIs internas.</p>
                         </li>
                         <li className="ms-4">
-                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+                            <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-azul-claro"></div>
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400">Irsis, Ago/2021 - Abr/2022</time>
                             <h3 className="text-lg font-semibold text-white">Suporte a Sistemas CRM</h3>
                             <p className="text-sm font-normal text-gray-400">Atuei realizando melhorias, manutenções, customizações e oferecendo suporte no sistema CRM Dynamics 365, atendendo às necessidades específicas de cada cliente.</p>
