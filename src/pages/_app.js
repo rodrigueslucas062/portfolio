8324eb72074d0610ce2ab03df41e6db64085c9da
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-07 20:49:09 -0300

    ➕ Sonner dependency

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.scss";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Toaster />
       <SpeedInsights />
       <Analytics />
     </>
