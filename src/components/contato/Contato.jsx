0e0552c38d4db53e39a065593ba81501cb5ea6be
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-07 21:05:10 -0300

    ♻️ Contact area refactor

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -1,37 +1,37 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
+import { toast } from "sonner";
 
 const Contato = () => {
-    return (
-        <section id="contato" className="bg-zinc-900 py-8">
-            <div className="md:hidden flex px-4 flex-col my-4 space-y-4 justify-around">
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
-                    <Github />
-                    <Link href={"https://github.com/rodrigueslucas062"}>Github</Link>
-                </div>
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
-                    <Mail />
-                    <span>rodrigueslucas062@gmail.com</span>
-                </div>
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
-                    <Linkedin />
-                    <Link href={"https://www.linkedin.com/in/rodrigueslucasdev/"}>LinkedIn</Link>
-                </div>
-            </div>
 
-            <div className="hidden md:flex my-4 justify-around">
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
-                    <Github />
-                    <Link href={"https://github.com/rodrigueslucas062"}>Github</Link>
-                </div>
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
+    const handleCopyLink = (text) => {
+        navigator.clipboard.writeText(text)
+        toast.success('Endereço de email copiado', {
+            position: 'bottom-center',
+            duration: 2000,
+        })
+    }
+
+    return (
+        <section id="contato" className="bg-zinc-900">
+            <div className="flex max-lg:flex-col max-lg:space-y-4 justify-around p-4 items-center lg:my-4">
+                <Link className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10 group max-lg:w-full"
+                    href={"https://github.com/rodrigueslucas062"}
+                    target="_blank" >
+                    <Github className="group-hover:bg-white group-hover:text-zinc-900 p-1 rounded-full" />
+                    Github
+                </Link>
+                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10 cursor-pointer max-lg:w-full"
+                    onClick={() => handleCopyLink('rodrigueslucas062@gmail.com')}>
                     <Mail />
                     <span>rodrigueslucas062@gmail.com</span>
                 </div>
-                <div className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10">
-                    <Linkedin />
-                    <Link href={"https://www.linkedin.com/in/rodrigueslucasdev/"}>LinkedIn</Link>
-                </div>
+                <Link className="flex gap-4 p-4 rounded-2xl bg-white/5 hover:bg-white/10 group max-lg:w-full"
+                    href={"https://www.linkedin.com/in/rodrigueslucasdev/"}
+                    target="_blank" >
+                    <Linkedin className="group-hover:bg-azul-claro p-1 rounded-md" />
+                    LinkedIn
+                </Link>
             </div>
         </section>
     )
