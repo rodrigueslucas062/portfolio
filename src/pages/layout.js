8d48dd3b852c7827e8b5c96af4734deb861c0903
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-11 22:26:28 -0300

    🎨 Head tag from Next.js

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,12 +1,13 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
     return (
         <html lang="pt">
-            <head>
+            <Head>
                 <title>Lucas Rodrigues</title>
-            </head>
+            </Head>
             <body>
                 {children}
                 <SpeedInsights />
