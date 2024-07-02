61a0d295bdc632447e1bc7ca028c0bc0ba84240b
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-07-01 23:34:15 -0300

    💄💫 New animated background

--- src/components/projetos/Projects.jsx
+++ src/components/projetos/Projects.jsx
@@ -1,4 +1,5 @@
 import { fetchFigmaData } from "@/pages/api/figma";
+import { BackgroundBeams } from "@/styles/animations/background-beams";
 import { useEffect, useState } from "react";
 
 export default function AllProjects() {
@@ -26,11 +27,12 @@ export default function AllProjects() {
     return (
         <section className="flex flex-col bg-zinc-900">
             {/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe> */}
-            <section id="expouna" className="flex bg-zinc-800 p-4">
+            <section id="expouna" className="flex bg-zinc-800 p-4 overflow-hidden">
                 <div className="w-1/2">
                     <span>
                         "Esse projeto foi desenvolvido para o trabalho final da matéria de Gerenciamento de Projetos da faculdade. Consiste em uma plataforma para disponibilizar os dados da apresentação das turmas de TI. Neste projeto tive a oportunidade de liderar uma pequena equipe de desenvolvimento, desenvolvi o layout no Figma e orientei meus colegas a desenvolver a aplicação, também desenvolvi algumas funcionalidades no site.",
                     </span>
+                    <BackgroundBeams />
                 </div>
                 <div className="w-1/2">
                     <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="100%" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe>
