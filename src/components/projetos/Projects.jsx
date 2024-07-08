788fc4e975b39b9d9e1a194ea2f325fb0f3185ba
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-07-08 00:09:21 -0300

    ✨Project page banner

--- src/components/projetos/Projects.jsx
+++ src/components/projetos/Projects.jsx
@@ -1,6 +1,9 @@
 import { fetchFigmaData } from "@/pages/api/figma";
 import { BackgroundBeams } from "@/styles/animations/background-beams";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import TrackVisibility from "react-on-screen";
+import Terreno from '../../../public/images/projects-banner/Terreno.svg'
 
 export default function AllProjects() {
     const [fileUrl, setFileUrl] = useState('');
@@ -25,20 +28,26 @@ export default function AllProjects() {
 
 
     return (
-        <section className="flex flex-col bg-zinc-900">
-            {/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe> */}
-            <section id="expouna" className="flex bg-zinc-800 p-4 overflow-hidden">
+        <>
+            <section className="h-screen bg-center-top bg-cover bg-no-repeat ceu" id="home">
+                <div className="h-screen bg-center-top bg-cover bg-no-repeat montanha">
+                    <div className="h-screen bg-center-top bg-cover bg-no-repeat terreno">
+
+                    </div>
+                </div>
+                {/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe> */}
+            </section>
+            {/* {/* <section id="expouna" className="flex bg-zinc-800 p-4 overflow-hidden"> */}
+            <section className="flex overflow-hidden">
                 <div className="w-1/2">
                     <span>
                         "Esse projeto foi desenvolvido para o trabalho final da matéria de Gerenciamento de Projetos da faculdade. Consiste em uma plataforma para disponibilizar os dados da apresentação das turmas de TI. Neste projeto tive a oportunidade de liderar uma pequena equipe de desenvolvimento, desenvolvi o layout no Figma e orientei meus colegas a desenvolver a aplicação, também desenvolvi algumas funcionalidades no site.",
                     </span>
-                    <BackgroundBeams />
                 </div>
                 <div className="w-1/2">
                     <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="100%" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe>
                 </div>
             </section>
-        </section>
-
+        </>
     );
 }
