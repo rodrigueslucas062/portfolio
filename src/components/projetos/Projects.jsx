eab68f1fab994bfa7da5707a81cb7990f6190b9c
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-07-08 22:17:21 -0300

    🤡Parallax test when scrolling the page

--- src/components/projetos/Projects.jsx
+++ src/components/projetos/Projects.jsx
@@ -1,9 +1,5 @@
 import { fetchFigmaData } from "@/pages/api/figma";
-import { BackgroundBeams } from "@/styles/animations/background-beams";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import TrackVisibility from "react-on-screen";
-import Terreno from '../../../public/images/projects-banner/Terreno.svg'
 
 export default function AllProjects() {
     const [fileUrl, setFileUrl] = useState('');
@@ -23,25 +19,43 @@ export default function AllProjects() {
         };
 
         loadFigmaData();
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const ceu = document.querySelector('.ceu');
+            const montanha = document.querySelector('.montanha');
+            const terreno = document.querySelector('.terreno');
+            const factor = 0.5;
+
+            if (ceu && montanha && terreno) {
+                ceu.style.height = `${100 - scrollPosition * factor}vh`;
+                montanha.style.height = `${100 - scrollPosition * factor}vh`;
+                terreno.style.height = `${100 - scrollPosition * factor}vh`;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     }, []);
-
-
 
     return (
         <>
-            <section className="h-screen bg-center-top bg-cover bg-no-repeat ceu" id="home">
-                <div className="h-screen bg-center-top bg-cover bg-no-repeat montanha">
-                    <div className="h-screen bg-center-top bg-cover bg-no-repeat terreno">
-
+            <section className="ceu" id="home">
+                <div className="montanha">
+                    <div className="terreno">
+                        <div className="text-center py-[15%]">
+                            <span className="text-8xl text-white font-semibold">Projetos</span>
+                        </div>
                     </div>
                 </div>
-                {/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FWVxv3zCVC91AkKkKsrSwmx%2FExpo-Una-prototype%3Fnode-id%3D0-1%26t%3DOKUDIt7KnEwDwq6u-1" allowfullscreen></iframe> */}
             </section>
-            {/* {/* <section id="expouna" className="flex bg-zinc-800 p-4 overflow-hidden"> */}
             <section className="flex overflow-hidden">
-                <div className="w-1/2">
+                <div className="w-1/2 p-8">
                     <span>
-                        "Esse projeto foi desenvolvido para o trabalho final da matéria de Gerenciamento de Projetos da faculdade. Consiste em uma plataforma para disponibilizar os dados da apresentação das turmas de TI. Neste projeto tive a oportunidade de liderar uma pequena equipe de desenvolvimento, desenvolvi o layout no Figma e orientei meus colegas a desenvolver a aplicação, também desenvolvi algumas funcionalidades no site.",
+                        "Esse projeto foi desenvolvido para o trabalho final da matéria de Gerenciamento de Projetos da faculdade. Consiste em uma plataforma para disponibilizar os dados da apresentação das turmas de TI. Neste projeto tive a oportunidade de liderar uma pequena equipe de desenvolvimento, desenvolvi o layout no Figma e orientei meus colegas a desenvolver a aplicação, também desenvolvi algumas funcionalidades no site."
                     </span>
                 </div>
                 <div className="w-1/2">
