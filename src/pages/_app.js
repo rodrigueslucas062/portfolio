e4d3afc9497bc2308ef7299f2877783ca0a9b51d
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-04-02 00:18:28 -0300

    ✨ Atualiza rota para ocultar a Navbar na página de leitura de currículo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const hideNavbarRoutes = ["/rodrigueslucas"];
+  const hideNavbarRoutes = ["/read.cv"];
 
   return (
     <>
